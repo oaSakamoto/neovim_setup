32bb9292e0ed6ac8636a224b183e4cee829d8c24
Olimpio Almeida Sakamoto <oasakamoto@proton.me>
2024-10-10 17:26:04 -0300

    feat: enhance code editing with Treesitter and autopair plugins
    - Add nvim-treesitter with configurations and dependencies
    - Include nvim-ts-autotag for tag autocompletion
    - Integrate ultimate-autopair.nvim for advanced bracket pairing
    - Update plugin versions in lazy-lock.json

---
+++ lua/plugins/treesitter/init.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      {'RRethy/nvim-treesitter-endwise',},
+      'nvim-treesitter/playground',
+    },
+    build = ':TSUpdate',
+    event = { 'bufReadPost', 'bufNewFile' },
+    opts = {
+      endwise = {
+        enable = true,
+      },
+      ensure_installed = {
+        'bash',
+        'dockerfile',
+        'html',
+        'markdown',
+        'markdown_inline',
+        'org',
+        'query',
+        'regex',
+        'latex',
+        'vim',
+        'vimdoc',
+        'yaml',
+        'lua',
+        'python'
+      },
+    },
+    config = function(_, opts)
+      require('nvim-treesitter.configs').setup(opts)
+    end
+  },
+  {
+    'windwp/nvim-ts-autotag',
+  },
+  {
+    'altermo/ultimate-autopair.nvim',
+    event =  { 'InsertEnter', 'CmdlineEnter' },
+    branch = 'v0.6',
+    opts = {}
+  },
+}
+
