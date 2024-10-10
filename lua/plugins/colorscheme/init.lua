96a888e12b3d60a97a747a0450c980751cd58db1
Olimpio Almeida Sakamoto <oasakamoto@proton.me>
2024-10-09 21:35:03 -0300

    feat: add Catppuccin color scheme configuration
    - Set up Catppuccin as the default color scheme
    - Configure transparent background
    - Customize highlight for @variable

--- lua/plugins/colorscheme/init.lua
+++ lua/plugins/colorscheme/init.lua
@@ -4,11 +4,13 @@ return {
     lazy = false,
     priority = 1000,
     flavor = 'mocha',
+    opts = {
+        transparent_background = true,
+    },
     config = function(_, opts)
         require('catppuccin').setup(opts)
         local colors = require('catppuccin.palettes').get_palette('mocha')
         vim.api.nvim_command('colorscheme catppuccin-mocha')
-        -- vim.cmd([[colorscheme catppuccin-mocha]])
 
         vim.api.nvim_set_hl(0, '@variable', { fg = colors.teal })
     end
