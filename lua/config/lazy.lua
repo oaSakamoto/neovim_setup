e5e08fa6febba075fd4eaf5ddf227305f9a36e9b
Olimpio Almeida Sakamoto <oasakamoto@proton.me>
2024-10-09 21:33:30 -0300

    fix: fix a typo

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,7 +1,14 @@
-local lazypath = vim.fn.stdpath('data') .. '/lazy.nvim'
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-    local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath})
+    local out = vim.fn.system({
+        'git',
+        'clone',
+        '--filter=blob:none',
+        '--branch=stable',
+        lazyrepo,
+        lazypath
+    })
     if vim.v.shell_error ~= 0 then
         vim.api.nvim_echo({
             { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
@@ -12,10 +19,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
         os.exit(1)
     end
 end
-vim.opt.rpt:prepend(lazypath)
+vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
     spec = {
-        { import = 'plugins' }
+        { import = 'plugins' },
     },
 })
---
+++ lua/plugins/colorscheme/init.lua
@@ -0,0 +1,15 @@
+return {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    lazy = false,
+    priority = 1000,
+    flavor = 'mocha',
+    config = function(_, opts)
+        require('catppuccin').setup(opts)
+        local colors = require('catppuccin.palettes').get_palette('mocha')
+        vim.api.nvim_command('colorscheme catppuccin-mocha')
+        -- vim.cmd([[colorscheme catppuccin-mocha]])
+
+        vim.api.nvim_set_hl(0, '@variable', { fg = colors.teal })
+    end
+}
