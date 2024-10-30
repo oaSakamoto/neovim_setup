eb68e01b8aad8e412aa664eb043fccdb1f4eeb22
Olimpio Almeida Sakamoto <oasakamoto@proton.me>
2024-10-30 02:46:33 -0300

    feat: add conform and config for format lua

---
+++ lua/plugins/conform/init.lua
@@ -0,0 +1,50 @@
+return {
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      '<leader>f',
+      function()
+        require('conform').format({ async = true, lsp_format = 'fallback' })
+      end,
+      mode = '',
+      desc = '[F]ormat buffer',
+    },
+  },
+  opts = {
+    notify_on_error = true,
+    format_on_save = function(bufnr)
+      local disable_filetypes = { c = true, cpp = true }
+      local lsp_format_opt
+      if disable_filetypes[vim.bo[bufnr].filetype] then
+        lsp_format_opt = 'never'
+      else
+        lsp_format_opt = 'fallback'
+      end
+      return {
+        timeout_ms = 500,
+        lsp_format = lsp_format_opt,
+      }
+    end,
+    formatters_by_ft = {
+      lua = { 'stylua' },
+    },
+    formatters = {
+      stylua = {
+        args = {
+          '--indent-type',
+          'Spaces',
+          '--indent-width',
+          '2',
+          '--quote-style',
+          'AutoPreferSingle',
+          '--search-parent-directories',
+          '--stdin-filepath',
+          '$FILENAME',
+          '-',
+        },
+      },
+    },
+  },
+}
